84dd3d0bc507fac9c1e7c0935bf79070c0261c05
seung611 <a46070766@gmail.com>
2026-09-22 18:45:50 +0900

    [Feat] Interface abstracrt 스크립트 생성

---
+++ Assets/Scripts/IInteractable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    // 플레이어와 아이템을 상호작용 할 수 있는
+}
---
+++ Assets/Scripts/ItemBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemBase : MonoBehaviour
+{
+    // psition
+    // Player와 상호작용
+}
