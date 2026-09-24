7188521b464604de03aaa4e133c270555bb6d0a8
jay <jayy5288@gmail.com>
2026-09-24 18:30:48 +0900

    [Feat] PuzzleC 키 아이템-키 홀 상호작용
    향후 매니저를 통해 전달하는 방법을 고안해야 합니다.

---
+++ Assets/Scripts/Puzzle_C_Scripts/GoalPoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using System;
+
+public class GoalPoint : MonoBehaviour
+{
+    [SerializeField] private string targetItem;
+    
+    public bool _isClearThisSecter = false;
+    
+    public event Action<bool> OnClearStateChanged;
+        
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetItem))
+        {
+            Debug.Log($"GoalPoint: {targetItem} entered.");
+            _isClearThisSecter = true;
+            OnClearStateChanged?.Invoke(_isClearThisSecter);
+            Debug.Log($"GoalPoint 판정: {_isClearThisSecter}");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(targetItem))
+        {
+            Debug.Log($"GoalPoint: {targetItem} exited.");
+        }
+    }
+}
