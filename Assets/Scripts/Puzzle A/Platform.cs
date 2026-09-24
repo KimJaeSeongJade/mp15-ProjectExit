d9f11a0a84ac3653ec6f8a0a0682aafd153b72da
Yeseul-0114 <ths0114@naver.com>
2026-09-24 18:00:35 +0900

    [Fix] 매니저 null ref 오류 수정

--- Assets/Scripts/Puzzle A/Platform.cs
+++ Assets/Scripts/Puzzle A/Platform.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    [Header("Platform Materials Settings")]
     [SerializeField] private Material _baseMat;
     [SerializeField] private Material _onStepMat;
+    [SerializeField] private Renderer[] _clearArray;
+    [SerializeField] private float _timeOut;
     
     private Renderer _renderer;
     private Coroutine _resetRoutine;
-  //  private bool _isBaseColor;
+    private Renderer[] _playerArray = new Renderer[6];
+    
+    
     
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
-        PuzzleA_Manager.Instance.OffColor();
+        OffColor();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,9 +28,29 @@ public class Platform : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("Platform : 퍼즐A 시작.");  // UI 로 띄워주면 좋겠다.
-            PuzzleA_Manager.Instance.OnColor();
+            OnColor();
         }
     }
+    public void OnColor()
+    {
+        _renderer.material = _onStepMat;
+        
+        // 타임아웃이 돌아가고 있으면 리셋시키는 코드
+        if (_resetRoutine != null)  
+        {
+            StopCoroutine(_resetRoutine);
+            _resetRoutine = null;
+        }
+        _resetRoutine = StartCoroutine(Timeout());
+    }
 
-   
+    public void OffColor()
+    {
+        _renderer.material = _baseMat;
+    }
+    private IEnumerator Timeout()
+    {
+        yield return new WaitForSeconds(_timeOut);
+        OffColor();
+    }
 }
--- Assets/Scripts/Puzzle A/PuzzleA_Manager.cs
+++ Assets/Scripts/Puzzle A/PuzzleA_Manager.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class PuzzleA_Manager : Singleton<PuzzleA_Manager>
 {
    [SerializeField] private Renderer[] _platforms;
-   [SerializeField] private float _timeOut;
    
    private Renderer _renderer;
    private Coroutine _resetRoutine;
    
-   [Header("Platform Materials Settings")]
-   private Material _baseMat;
-   private Material _onStepMat;
+   
    
    
    private void Awake()
@@ -24,7 +21,7 @@ public class PuzzleA_Manager : Singleton<PuzzleA_Manager>
    {
       for (int i = 0; i < _platforms.Length; i++)
       {
-         _platforms[i].material = _baseMat;
+         // _platforms[i].material = ;
       }
       if (_resetRoutine != null)
       {
@@ -33,27 +30,7 @@ public class PuzzleA_Manager : Singleton<PuzzleA_Manager>
       }
    }
    
-   public void OnColor()
-   {
-      _renderer.material = _onStepMat;
-        
-      // 타임아웃이 돌아가고 있으면 리셋시키는 코드
-      if (_resetRoutine != null)  
-      {
-         StopCoroutine(_resetRoutine);
-         _resetRoutine = null;
-      }
-      _resetRoutine = StartCoroutine(Timeout());
-   }
+  
 
-   public void OffColor()
-   {
-      _renderer.material = _baseMat;
-   }
-
-   private IEnumerator Timeout()
-   {
-      yield return new WaitForSeconds(_timeOut);
-      OffColor();
-   }
+  
 }
