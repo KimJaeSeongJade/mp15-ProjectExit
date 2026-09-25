6119b8012dd5a76edfa049bb31b2bc059045d7d3
jay <jayy5288@gmail.com>
2026-09-25 13:52:30 +0900

    [Feat] 게임 진입 시 How To Play에 대한 작업 중

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InGameUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _howToPlayUI;
+    [SerializeField] private GameObject _pauseQUI;
+    [SerializeField] private GameObject _clearUI;
+    [SerializeField] private GameObject _deadUI;
+    [SerializeField] private GameObject _inGameUI;
+    [SerializeField] private Button _howToPlayUIStartButton;
+
+
+    private void OnEnable() => BindButtonEvents();
+    private void OnDisable() => UnbindButtonEvents();
+
+    private void LateUpdate()
     {
-        
+        //PressToPlay();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void BindButtonEvents()
     {
+        _howToPlayUIStartButton.onClick.AddListener(Init);
+    }
+
+    private void UnbindButtonEvents()
+    {
+        _howToPlayUIStartButton.onClick.RemoveListener(Init);
+    }
+
+    private void PressToPlay()
+    {
+        _howToPlayUI.SetActive(false);
+        Time.timeScale = 1f;
+        _inGameUI.SetActive(true);
+    }
+    
+    private void Init()
+    {
+        _howToPlayUI.SetActive(true);
+        Time.timeScale = 0f;
         
+        _pauseQUI.SetActive(false);
+        _clearUI.SetActive(false);
+        _deadUI.SetActive(false);
+        _inGameUI.SetActive(false);
     }
 }
