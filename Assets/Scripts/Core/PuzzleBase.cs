b56b1eea041fcb38754b88ce83f3f6e92657568e
Jade <jade16work@gmail.com>
2026-09-25 18:55:13 +0900

    [Feat] PuzzleBase 클래스 추가. 기존 퍼즐 상속

---
+++ Assets/Scripts/Core/PuzzleBase.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PuzzleBase : MonoBehaviour
+{
+    public bool IsClear { get; protected set; } = false;
+}
