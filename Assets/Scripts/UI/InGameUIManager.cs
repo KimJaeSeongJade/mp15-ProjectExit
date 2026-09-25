11df39db34b0bfa2f5b701b26f2a64d0c45ba335
jay <jayy5288@gmail.com>
2026-09-25 18:13:43 +0900

    [Feat] 게임 일시정지 후 Continue를 눌렀을 때 재개 추가

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -35,7 +35,7 @@ public class InGameUIManager : MonoBehaviour
     private void BindButtonEvents()
     {
         _howToPlayUIStartButton.onClick.AddListener(PressToPlay);
-        _pauseQUIContinueButton.onClick.AddListener(OnGamePause);
+        _pauseQUIContinueButton.onClick.AddListener(OnClickContinue);
         _pauseQUIExitButton.onClick.AddListener(OnGamePause);
     }
 
@@ -43,6 +43,7 @@ public class InGameUIManager : MonoBehaviour
     {
         GameManager.Instance.OnGameStart += OnGameStart;
         GameManager.Instance.OnGamePause += OnGamePause;
+        GameManager.Instance.OnGameResume += OnGameResume;
 
     }
 
@@ -50,17 +51,19 @@ public class InGameUIManager : MonoBehaviour
     {
         GameManager.Instance.OnGameStart -= OnGameStart;
         GameManager.Instance.OnGamePause -= OnGamePause;
+        GameManager.Instance.OnGameResume -= OnGameResume;
     }
 
     private void UnbindButtonEvents()
     {
         _howToPlayUIStartButton.onClick.RemoveListener(PressToPlay);
-        _pauseQUIContinueButton.onClick.RemoveListener(OnGamePause);
+        _pauseQUIContinueButton.onClick.RemoveListener(OnClickContinue);
         _pauseQUIExitButton.onClick.RemoveListener(OnGamePause);
     }
 
     private void BeforePlaying() =>GameManager.Instance.PauseGame();
     private void PressToPlay() => GameManager.Instance.StartGame();
+    private void OnClickContinue() => GameManager.Instance.ResumeGame();
     
     private void OnGameStart()
     {
@@ -131,9 +134,9 @@ public class InGameUIManager : MonoBehaviour
         }
     }
     
-    // + 
+    // + InGamePause 관련
     [SerializeField] private KeyCode _pauseKey = KeyCode.Q;
-    private bool _isPressedPauseKey=>Input.GetKeyDown(_pauseKey);
+    private bool _isPressedPauseKey => Input.GetKeyDown(_pauseKey);
     
     [SerializeField] private Button _pauseQUIExitButton;
     [SerializeField] private Button _pauseQUIContinueButton;
