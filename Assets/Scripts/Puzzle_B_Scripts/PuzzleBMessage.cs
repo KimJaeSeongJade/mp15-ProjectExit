12567ea784b8c7f703ccae75ccc68222306f427c
Makom64 <yellowffd400@gmail.com>
2026-09-25 16:45:32 +0900

    [Feat] 클리어 UI 구현, 카메라 처다보기 구현

--- Assets/Scripts/Puzzle_B_Scripts/PuzzleBMessage.cs
+++ Assets/Scripts/Puzzle_B_Scripts/PuzzleBMessage.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleBMessage : MonoBehaviour
-{ 
+{
+    [SerializeField] private Transform _playerCam; // 메인카메라 참조
     private void Awake() => Init();
 
+    private void Update() => SetRotation();
+    
     private void Init()
     {
         transform.gameObject.SetActive(false);
     }
 
-    private void PopOut()
+    private void SetRotation()
     {
-        
+        transform.LookAt(_playerCam);
     }
 }
