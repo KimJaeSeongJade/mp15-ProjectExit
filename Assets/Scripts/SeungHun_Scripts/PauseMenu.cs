c2981b561511d5003662c68b7c58ed47bc40e7d1
Makom64 <yellowffd400@gmail.com>
2026-09-25 14:08:13 +0900

    [Feat] 브랜치 생성

--- Assets/Scripts/SeungHun_Scripts/PauseMenu.cs
+++ Assets/Scripts/SeungHun_Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -23,6 +24,7 @@ public class PauseMenu : MonoBehaviour
     private void BindButtonEvents()
     {
         _continue.onClick.AddListener(HidePanel);
+        _exit
     }
 
     private void UnBindButtonEvents()
@@ -33,5 +35,10 @@ public class PauseMenu : MonoBehaviour
     private void HidePanel()
     {
         gameObject.SetActive(false);
+    }
+
+    private void GoToTitle()
+    {
+        SceneManager.LoadScene("GameTitle");
     }
 }
