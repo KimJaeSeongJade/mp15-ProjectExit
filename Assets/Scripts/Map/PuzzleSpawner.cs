5dc62ac107c2f2ae61c4b3969f03bae5c3e84432
seung611 <a46070766@gmail.com>
2026-09-26 18:01:10 +0900

    [Feat] 몬스터 프리팹 생성

--- Assets/Scripts/Map/PuzzleSpawner.cs
+++ Assets/Scripts/Map/PuzzleSpawner.cs
@@ -11,7 +11,7 @@ public class SpawnerPoint : MonoBehaviour
     [SerializeField] private Transform _puzzleCSpawnPoint;
 
     [SerializeField] private GameObject _player;
-    // [SerializeField] private GameObject _monster;
+    [SerializeField] private GameObject _monster;
     
     [SerializeField] private GameObject _puzzleA;
     [SerializeField] private GameObject _puzzleB;
@@ -22,7 +22,7 @@ public class SpawnerPoint : MonoBehaviour
     private void Spawn()
     {
         Instantiate(_player, PlayerSpawnPoint.position, Quaternion.identity);
-        // Instantiate(_monster, MonsterSpawnPoint.position, Quaternion.identity);
+        Instantiate(_monster, MonsterSpawnPoint.position, Quaternion.identity);
         Instantiate(_puzzleA, _puzzleASpawnPoint.position, Quaternion.identity); 
         Instantiate(_puzzleB, _puzzleBSpawnPoint.position, Quaternion.identity);
         Instantiate(_puzzleC, _puzzleCSpawnPoint.position, Quaternion.identity);
