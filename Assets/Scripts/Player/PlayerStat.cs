8454a3d1c1581a10400a4f4f0decbcd56897163a
Jay <jayy5288@gmail.com>
2026-09-27 03:58:59 +0900

    [Feat] 플레이어 Hp UI 연결

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerStat : MonoBehaviour
 {
-    [field: SerializeField] public int PlayerHealth { get; set; } = 100;
+    [field: SerializeField] public int MaxHealth { get; set; } = 100;
     [field: SerializeField] public float MoveSpeed { get; set; } = 5f;
 
     [field: SerializeField] public float InteractRange { get; set; } = 10f;
+    
+    // + 추가
+    private int _playerHealth;
+    public int PlayerHealth
+    {
+        get => _playerHealth;
+        set
+        {
+            _playerHealth = Mathf.Clamp(value, 0, MaxHealth);
+            OnHealthChanged?.Invoke(_playerHealth, MaxHealth);
+        }
+    }
+
+    public event Action<int, int> OnHealthChanged;
+
+    private void Awake()
+    {
+        PlayerHealth = MaxHealth; // 초기화하면서 최초 이벤트도 한 번 발생시킴
+    }
 }
